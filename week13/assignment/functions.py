92c48729d045f386291fb06a3c4130d13610041f
Erick <moralerick90@gmail.com>
2025-04-04 20:42:53 -0700

    added multithreaded solutions to dfs and bfs functions

--- week13/assignment/functions.py
+++ week13/assignment/functions.py
@@ -15,21 +15,16 @@ person = Request_thread(f'{TOP_API_URL}/person/{id}')
 """
 from common import *
 import threading
+import requests
 import queue
-import concurrent.futures
-
-# Shared Data Structures
-visited = set()  # Keeps track of processed person IDs
-visited_lock = threading.Lock()  # Synchronization lock
-stack = queue.LifoQueue()  # Thread-safe DFS stack
-
+from collections import deque
 
 # -----------------------------------------------------------------------------
 def depth_fs_pedigree(family_id, tree):
     # TODO - implement Depth first retrieval
 
-    stack = [family_id]  # Stack for DFS
     visited_families = set()
+    stack = [family_id]
 
     while stack:
         current_family_id = stack.pop()
@@ -38,54 +33,101 @@ def depth_fs_pedigree(family_id, tree):
             continue
         visited_families.add(current_family_id)
 
-        # Request family data from the server
-        family_request = Request_thread(f'{TOP_API_URL}/family/{current_family_id}')
-        family_request.start()
-        family_request.join()
-        if not family_request.response:
+        # Request the family
+        family_thread = Request_thread(f'{TOP_API_URL}/family/{current_family_id}')
+        family_thread.start()
+        family_thread.join()
+
+        if not family_thread.response:
+            print(f'[ERROR] No data received for family {current_family_id}')
             continue
 
-        family = Family(current_family_id, family_request.response)
+        family_data = family_thread.response
+        family = Family(current_family_id, family_data)
         tree.add_family(family)
 
-        # Retrieve and add husband
-        if family.husband:
-            husband_request = Request_thread(f'{TOP_API_URL}/person/{family.husband}')
-            husband_request.start()
-            husband_request.join()
-            if husband_request.response:
-                husband = Person(husband_request.response)
-                tree.add_person(husband)
-                if husband.parents:  # Push husband's parent family to stack
-                    stack.append(husband.parents)
+        # Fetch husband and wife
+        person_threads = []
+        for person_id in [family.husband, family.wife]:
+            thread = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+            thread.start()
+            person_threads.append((person_id, thread))
 
-        # Retrieve and add wife
-        if family.wife:
-            wife_request = Request_thread(f'{TOP_API_URL}/person/{family.wife}')
-            wife_request.start()
-            wife_request.join()
-            if wife_request.response:
-                wife = Person(wife_request.response)
-                tree.add_person(wife)
-                if wife.parents:  # Push wife's parent family to stack
-                    stack.append(wife.parents)
-
-        # Retrieve and add children
+        # Fetch children
         for child_id in family.children:
-            child_request = Request_thread(f'{TOP_API_URL}/person/{child_id}')
-            child_request.start()
-            child_request.join()
-            if child_request.response:
-                child = Person(child_request.response)
-                tree.add_person(child)
+            thread = Request_thread(f'{TOP_API_URL}/person/{child_id}')
+            thread.start()
+            person_threads.append((child_id, thread))
+
+        # Join all person threads and add to tree
+        for person_id, thread in person_threads:
+            thread.join()
+            if thread.response:
+                person = Person(thread.response)
+                tree.add_person(person)
+
+        # Add parents of husband and wife to stack (for DFS)
+        for person_id in [family.husband, family.wife]:
+            person = tree.get_person(person_id)
+            if person:
+                parent_family_id = person.parents
+                if parent_family_id and parent_family_id not in visited_families:
+                    stack.append(parent_family_id)
 
 # -----------------------------------------------------------------------------
 def breadth_fs_pedigree(start_id, tree):
     # TODO - implement breadth first retrieval
 
-    print('WARNING: BFS function not written')
+    visited_families = set()
+    queue = deque([start_id])
 
-    pass
+    while queue:
+        current_family_id = queue.popleft()
+
+        if current_family_id in visited_families:
+            continue
+        visited_families.add(current_family_id)
+
+        # Request the family
+        family_thread = Request_thread(f'{TOP_API_URL}/family/{current_family_id}')
+        family_thread.start()
+        family_thread.join()
+
+        if not family_thread.response:
+            print(f'[ERROR] No data received for family {current_family_id}')
+            continue
+
+        family_data = family_thread.response
+        family = Family(current_family_id, family_data)
+        tree.add_family(family)
+
+        # Fetch husband and wife
+        person_threads = []
+        for person_id in [family.husband, family.wife]:
+            thread = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+            thread.start()
+            person_threads.append((person_id, thread))
+
+        # Fetch children
+        for child_id in family.children:
+            thread = Request_thread(f'{TOP_API_URL}/person/{child_id}')
+            thread.start()
+            person_threads.append((child_id, thread))
+
+        # Join and store people
+        for person_id, thread in person_threads:
+            thread.join()
+            if thread.response:
+                person = Person(thread.response)
+                tree.add_person(person)
+
+        # Enqueue parents of husband and wife
+        for person_id in [family.husband, family.wife]:
+            person = tree.get_person(person_id)
+            if person:
+                parent_family_id = person.parents
+                if parent_family_id and parent_family_id not in visited_families:
+                    queue.append(parent_family_id)
 
 
 # -----------------------------------------------------------------------------
@@ -93,6 +135,71 @@ def breadth_fs_pedigree_limit5(start_id, tree):
     # TODO - implement breadth first retrieval
     #      - Limit number of concurrent connections to the FS server to 5
 
-    print('WARNING: BFS (Limit of 5 threads) function not written')
+    max_threads = 5
+    semaphore = threading.Semaphore(max_threads)
+    visited_families = set()
+    queue = deque([start_id])
+    lock = threading.Lock()
 
-    pass
+    def fetch_family_and_people(family_id):
+        nonlocal tree, visited_families, queue
+
+        # Acquire semaphore slot
+        with semaphore:
+            # Get family info
+            family_thread = Request_thread(f'{TOP_API_URL}/family/{family_id}')
+            family_thread.start()
+            family_thread.join()
+
+            if not family_thread.response:
+                print(f'[ERROR] No family data for {family_id}')
+                return
+
+            family_data = family_thread.response
+            family = Family(family_id, family_data)
+
+            with lock:
+                if family_id not in visited_families:
+                    tree.add_family(family)
+                    visited_families.add(family_id)
+
+            # Fetch husband, wife, and children
+            person_threads = []
+            for person_id in [family.husband, family.wife] + family.children:
+                person_thread = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+                person_thread.start()
+                person_threads.append((person_id, person_thread))
+
+            # Join threads and add people to tree
+            for person_id, thread in person_threads:
+                thread.join()
+                if thread.response:
+                    person = Person(thread.response)
+                    with lock:
+                        tree.add_person(person)
+
+            # Enqueue parents (only once per family)
+            for person_id in [family.husband, family.wife]:
+                person = tree.get_person(person_id)
+                if person and person.parents not in visited_families and person.parents is not None:
+                    with lock:
+                        queue.append(person.parents)
+
+    # Start thread-processing loop
+    threads = []
+
+    while queue:
+        family_id = queue.popleft()
+
+        # Skip if already processed
+        if family_id in visited_families:
+            continue
+
+        # Start a thread that uses semaphore
+        t = threading.Thread(target=fetch_family_and_people, args=(family_id,))
+        t.start()
+        threads.append(t)
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
